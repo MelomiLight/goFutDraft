862089be6394dbad868a82e66f9af27c4f331fe2
MelomiLight <97735001+MelomiLight@users.noreply.github.com>
2023-02-24 01:59:18 +0600

    some updates

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,20 +20,20 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/futdraft", app.futDraftHandler)
 	router.HandlerFunc(http.MethodGet, "/futdraft/choose", app.futDraftChooseHandler)
 
-  router.HandlerFunc(http.MethodGet,"/futDraft/players", app.ListPlayersHandler)
-	router.HandlerFunc(http.MethodGet,"/futDraft/players/:id", app.GetPlayerHandler)
-	router.HandlerFunc(http.MethodPost,"/futDraft/players", app.CreatePlayerHandler)
-	router.HandlerFunc(http.MethodDelete,"/futDraft/players/:id", app.DeletePlayerHandler)
-	router.HandlerFunc(http.MethodPut,"/futDraft/players/:id", app.UpdatePlayerHandler)
+  router.HandlerFunc(http.MethodGet,"/futdraft/players", app.ListPlayersHandler)
+	router.HandlerFunc(http.MethodGet,"/futdraft/players/:id", app.GetPlayerHandler)
+	router.HandlerFunc(http.MethodPost,"/futdraft/players", app.CreatePlayerHandler)
+	router.HandlerFunc(http.MethodDelete,"/futdraft/players/:id", app.DeletePlayerHandler)
+	router.HandlerFunc(http.MethodPut,"/futdraft/players/:id", app.UpdatePlayerHandler)
 
-	router.HandlerFunc(http.MethodGet,"/futDraft/clubs", app.ListClubsHandler)
-	router.HandlerFunc(http.MethodGet,"/futDraft/clubs/:id", app.GetClubHandler)
+	router.HandlerFunc(http.MethodGet,"/futdraft/clubs", app.ListClubsHandler)
+	router.HandlerFunc(http.MethodGet,"/futdraft/clubs/:id", app.GetClubHandler)
 
-	router.HandlerFunc(http.MethodGet,"/futDraft/leagues", app.ListLeaguesHandler)
-	router.HandlerFunc(http.MethodGet,"/futDraft/leagues/:id", app.GetLeagueHandler)
+	router.HandlerFunc(http.MethodGet,"/futdraft/leagues", app.ListLeaguesHandler)
+	router.HandlerFunc(http.MethodGet,"/futdraft/leagues/:id", app.GetLeagueHandler)
 	
-	router.HandlerFunc(http.MethodGet,"/futDraft/nations", app.ListNationsHandler)
-	router.HandlerFunc(http.MethodGet,"/futDraft/nations/:id", app.GetNationHandler)
+	router.HandlerFunc(http.MethodGet,"/futdraft/nations", app.ListNationsHandler)
+	router.HandlerFunc(http.MethodGet,"/futdraft/nations/:id", app.GetNationHandler)
 
 	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
 }
--- go.mod
+++ go.mod
@@ -10,4 +10,4 @@ require (
 
 require github.com/lib/pq v1.10.7
 
-require github.com/gorilla/mux v1.8.0 // indirect
+
